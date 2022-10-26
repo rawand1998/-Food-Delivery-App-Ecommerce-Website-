da25c37c5cac8fd6d34d57c7fe36a35b92ca01b6
rawand1998 <rawandgaradh@gmail.com>
2022-10-26 15:39:11 +0300

    navbar component

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Router from '../../routes/Router'
 import {Route,Routes} from 'react-router-dom'
+import NavBar from '../NavBar/NavBar'
 function Layout() {
   return (
     <div>
+      <NavBar/>
       <Routes>
       {Router.routes.map((route, Index) => (
           <Route path={route.path} element={route.element} key={Index} />
