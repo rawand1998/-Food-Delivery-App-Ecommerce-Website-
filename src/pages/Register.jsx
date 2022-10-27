bec7a4a898a53c820fdf51d0a357f0c05edbbddc
rawand1998 <rawandgaradh@gmail.com>
2022-10-27 16:06:41 +0300

    register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Footer from '../component/Footer/Footer'
+import {Link} from 'react-router-dom'
 function Register() {
   return (
-    <div>Register</div>
+    <div>
+    <div className="login_image">
+      <h2>Signup</h2>
+    </div>
+    <form action="" className="auth register">
+      <input type="text" placeholder="Full Name" />
+      <input type="text" placeholder="Email" />
+      <input type="text" placeholder="Password" />
+      <button className="auth_btn">Register</button>
+    </form>
+    <Link className="login_para" to="/login">Already have an account? Login</Link>
+    <Footer />
+  </div>
   )
 }
 
