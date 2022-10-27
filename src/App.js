41c3ecdccc5c970d9b61fe4932f01adaff3f9111
rawand1998 <rawandgaradh@gmail.com>
2022-10-27 14:07:16 +0300

    food category

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './styles/index';
 import Layout from './component/Layout/Layout';
-import Footer from './component/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-   {/* <Layout/> */}
-  <Footer/>
+   <Layout/>
     </div>
   );
 }
